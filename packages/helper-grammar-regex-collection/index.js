16117fb485bbb5f8262c0fc719b15ee72e07cc76
Stefan Buck <github@stefanbuck.com>
2016-04-24 01:01:13 +0200

    WIP add custom modal

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -2,10 +2,12 @@ const REQUIRE = /require(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const REQUIRE_RESOLVE = /require(?:.resolve)(?:\s|\()(['"][^'"\s]+['"])\)?/g;
 const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
 const GEM = /gem (['"][^'"\s]+['"])/g;
+const TYPESCRIPT_REFERENCE = /\/{3}\s?<reference path=['"]([^'"\s]+)['"]/g;
 
 export {
   REQUIRE,
   REQUIRE_RESOLVE,
   IMPORT,
   GEM,
+  TYPESCRIPT_REFERENCE,
 };
