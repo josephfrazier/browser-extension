0b9be0cae2acc378bb0a8dbd40fce450be1c2671
Joseph Frazier <1212jtraceur@gmail.com>
2016-08-04 19:16:28 -0400

    Use `concat-regexp` to extract QUOTED_WORD pattern from regexes
    This makes it easier to read the expressions and understand how to add
    new ones.

--- packages/helper-grammar-regex-collection/index.js
+++ packages/helper-grammar-regex-collection/index.js
@@ -1,11 +1,14 @@
-const REQUIRE = /require(?:\.resolve)?(?:\s|\()(['"][^'"\s]+['"])\)?/g;
-const IMPORT = /import [\r\n\s\w{},*\$]*(?: from )?(['"][^'"\s]+['"])/g;
-const EXPORT = /export [\r\n\s\w{},*\$]*(?: from )(['"][^'"\s]+['"])/g;
-const GEM = /gem (['"][^'"\s]+['"])/g;
-const HOMEBREW = /(?:depends_on|conflicts_with)(?: cask:| formula:)? (['"][^'"\s]+['"])/g;
-const TYPESCRIPT_REFERENCE = /\/{3}\s?<reference path=(['"][^'"\s]+['"])/g;
+import concatRegexp from 'concat-regexp';
+
+const QUOTED_WORD = /(['"][^'"\s]+['"])/;
+const REQUIRE = concatRegexp(/require(?:\.resolve)?(?:\s|\()/, QUOTED_WORD, /\)?/g);
+const IMPORT = concatRegexp(/import [\r\n\s\w{},*\$]*(?: from )?/, QUOTED_WORD, /(?:)/g);
+const EXPORT = concatRegexp(/export [\r\n\s\w{},*\$]*(?: from )/, QUOTED_WORD, /(?:)/g);
+const GEM = concatRegexp(/gem /, QUOTED_WORD, /(?:)/g);
+const HOMEBREW = concatRegexp(/(?:depends_on|conflicts_with)(?: cask:| formula:)? /, QUOTED_WORD, /(?:)/g);
+const TYPESCRIPT_REFERENCE = concatRegexp(/\/{3}\s?<reference path=/, QUOTED_WORD, /(?:)/g);
 const DOCKER_FROM = /FROM\s([^\n]*)/g;
-const VIM_PLUGIN = /(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\s(['"][^'"\s]+['"])/g;
+const VIM_PLUGIN = concatRegexp(/(?:(?:(?:Neo)?Bundle(?:Lazy|Fetch)?)|Plug(?:in)?)\s/, QUOTED_WORD, /(?:)/g);
 const RUST_CRATE = /(?:extern crate|use) ([^:; ]+)/g;
 const PYTHON_IMPORT = /(?:(?:\n|^)\s*import|from)\s([^\s]*)/g;
 
